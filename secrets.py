201253fa7a8291924cba33ab889b411536adcec0
shira-apiiro <145326526+shira-apiiro@users.noreply.github.com>
2025-07-23 17:27:56 +0300

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,26 @@
+import requests
+
+password = "AIzaSyCzmABOfcTbibaP_VmZPsFn0q3Ro2sXlHQ"
+
+pwd = "AIzaSyDssABOfcTbibaP_VnZPhFn0q3Ro2sXlSQ"
+
+response = requests.get('http://google.com', {'password': password})
+
+Token: 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
+
+pass = "AIzaSyDzmABOfcTbibcP_VnZPsFn0q2Ro2sXlSQ"
+
+pass = "AIzaSyDzmABOfcTbibcP_VnZPsFn0q2Ro1iGuHS"
+token= "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"
+
+AUTOFILL_HINT_WIFI_PASSWORD = "Password"
+
+password = "0123455589055";
+
+pwd = "AIzaSyDssABOfcTbibaP_VnZPhFn0q3Ro2sXsSQ"
+pwd = "AIzaSyDssABOfcTbibaP_VnZPhFn0q3Ro2sShSQ"
+
+pass = "AIzaSyDzmABOfcTbibcP_VnZPhFn0q3Ro3sXlSw"
+password = "AIzaSyDzmABOfcTbibcP_VnZPhFn0q3Ro3sXlSw"
+pass: "AIzaSyDzmABOfcTbibcP_VnZPsFn0q2Ro2sXlSw"
+password = "AIzaSyCzmABOfcTbibaP_VmZPsFn0q3Ro2sXlHw"
